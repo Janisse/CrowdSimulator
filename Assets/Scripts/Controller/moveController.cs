93d5e35293caf7598529308aaa4dbec278de0f63
sexysoushi <barbara1603@hotmail.com>
2014-04-02 20:54:19 +0200

    Architecture des classes + script spawn ok

---
+++ Assets/Scripts/Controller/moveController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using MiddleVR_Unity3D;
+
+public class moveController : MonoBehaviour {
+
+	//variables
+	public float rotationSpeed;
+	public float moveSpeed;
+	public bool canJump;
+	public float jumpSpeed;
+	public float sensibilite;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Rotation
+		float rotationAxis = MiddleVR.VRDeviceMgr.GetJoystick ().GetAxisValue (3);
+		//Regle le probleme d'hypersensibilite du controller
+		if(rotationAxis>sensibilite || rotationAxis<-sensibilite)
+			transform.Rotate(new Vector3 (0, rotationAxis, 0) * rotationSpeed);
+
+		//Translation
+		float translationX = MiddleVR.VRDeviceMgr.GetJoystick().GetAxisValue(0);
+		float translationZ = -MiddleVR.VRDeviceMgr.GetJoystick().GetAxisValue(1);
+		//Regle le probleme d'hypersensibilite du controller
+		if((translationX>sensibilite || translationX<-sensibilite) || (translationZ>sensibilite || translationZ<-sensibilite))
+			transform.Translate(new Vector3 (translationX, 0, translationZ) * moveSpeed);
+
+
+		//Jump
+		if(MiddleVR.VRDeviceMgr.IsKeyPressed(MiddleVR.VRK_SPACE) == true)
+		{
+			rigidbody.AddForce(Vector3.up * jumpSpeed);
+		}
+	}
+}
---
+++ Assets/Scripts/Walking Crowd/Spawn.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour
+{
+	public int minPeople = 1;
+	public int nbPeople;
+	public int percentSlider = 6;
+
+	public GameObject prefab_people;
+	public GameObject floor;
+
+	public Vector3 _posNewPeople;
+	Vector3 _minGround = new Vector3(2,2,2);
+	Vector3 _dimGround = new Vector3(20,20,20);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void peopleSpawn()
+	{
+		// nombre aléatoire de gens par rapport au pourcentage du slider
+		//nbPeople = Random.Range(minPeople, percentSlider);
+		nbPeople = 10;
+		
+		//création des gens aléatoirement sur la map
+		for(int i=0 ; i<nbPeople ; ++i)
+		{
+
+	// il faut faire des zones de spawn comme devant les magasins par exemple
+			//pour l'instant spawn aléatoirement sur la map
+			_posNewPeople.x = _minGround.x + Random.value * _dimGround.x;
+			_posNewPeople.y = 1;
+			_posNewPeople.z = _minGround.z + Random.value * _dimGround.z;
+			
+			Object newPeople;
+			newPeople = Instantiate(prefab_people, _posNewPeople, prefab_people.transform.rotation);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
