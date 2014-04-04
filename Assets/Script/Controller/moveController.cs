6d6445e0dc97f7c283b663a9404f6e34376ca9a8
Janisse <j_clement_42@live.fr>
2014-04-04 10:26:42 -0400

    - Ajout d'un menu (v1)  - Reglage sensibilité camera par le menu

--- Assets/Script/Controller/moveController.cs
+++ Assets/Script/Controller/moveController.cs
@@ -11,61 +11,71 @@ public class moveController : MonoBehaviour {
 	public float jumpSpeed;
 	public float sensibilite;
 	public bool isControllerPlugIn;
+	public bool pause;
 
 	// Use this for initialization
 	void Start () {
+		pause = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(isControllerPlugIn == true)
+		if(pause == false)
 		{
-			//Rotation
-			float rotationAxis = MiddleVR.VRDeviceMgr.GetJoystick ().GetAxisValue (3);
-			//Regle le probleme d'hypersensibilite du controller
-			if(rotationAxis>sensibilite || rotationAxis<-sensibilite)
-				transform.Rotate(new Vector3 (0, rotationAxis, 0) * rotationSpeed);
-
-			//Translation
-			float translationX = MiddleVR.VRDeviceMgr.GetJoystick().GetAxisValue(0);
-			float translationZ = -MiddleVR.VRDeviceMgr.GetJoystick().GetAxisValue(1);
-			//Regle le probleme d'hypersensibilite du controller
-			if((translationX>sensibilite || translationX<-sensibilite) || (translationZ>sensibilite || translationZ<-sensibilite))
-				transform.rigidbody.AddRelativeForce(new Vector3 (translationX, 0, translationZ) * moveSpeed);
-			else
-				transform.rigidbody.velocity = new Vector3 (transform.rigidbody.velocity.x * 0.5f, transform.rigidbody.velocity.y * 1.1f, transform.rigidbody.velocity.z * 0.5f);
-
-
-			//Jump
-			if(MiddleVR.VRDeviceMgr.IsKeyPressed(MiddleVR.VRK_SPACE) == true)
+			if(isControllerPlugIn == true)
 			{
-				rigidbody.AddForce(Vector3.up * jumpSpeed);
+				//Rotation
+				float rotationAxis = MiddleVR.VRDeviceMgr.GetJoystick ().GetAxisValue (3);
+				//Regle le probleme d'hypersensibilite du controller
+				if(rotationAxis>sensibilite || rotationAxis<-sensibilite)
+					transform.Rotate(new Vector3 (0, rotationAxis, 0) * rotationSpeed/50);
+
+				//Translation
+				float translationX = MiddleVR.VRDeviceMgr.GetJoystick().GetAxisValue(0);
+				float translationZ = -MiddleVR.VRDeviceMgr.GetJoystick().GetAxisValue(1);
+				//Regle le probleme d'hypersensibilite du controller
+				if((translationX>sensibilite || translationX<-sensibilite) || (translationZ>sensibilite || translationZ<-sensibilite))
+					transform.rigidbody.AddRelativeForce(new Vector3 (translationX, 0, translationZ) * moveSpeed);
+				else
+					transform.rigidbody.velocity = new Vector3 (transform.rigidbody.velocity.x * 0.5f, transform.rigidbody.velocity.y * 1.1f, transform.rigidbody.velocity.z * 0.5f);
+
+
+				//Jump
+				if(MiddleVR.VRDeviceMgr.IsKeyPressed(MiddleVR.VRK_SPACE) == true)
+				{
+					rigidbody.AddForce(Vector3.up * jumpSpeed);
+				}
+			}
+			else
+			{
+				//Rotation
+				float rotationAxis = Input.GetAxis("Mouse X");
+				Debug.Log(rotationAxis.ToString());
+				//Regle le probleme d'hypersensibilite du controller
+				if(rotationAxis>sensibilite || rotationAxis<-sensibilite)
+					transform.Rotate(new Vector3 (0, rotationAxis, 0) * rotationSpeed/50);
+				
+				//Translation
+				float translationX = Input.GetAxis("Horizontal");
+				float translationZ = Input.GetAxis("Vertical");
+				//Regle le probleme d'hypersensibilite du controller
+				if((translationX>sensibilite || translationX<-sensibilite) || (translationZ>sensibilite || translationZ<-sensibilite))
+					transform.rigidbody.AddRelativeForce(new Vector3 (translationX, 0, translationZ) * moveSpeed);
+				else
+					transform.rigidbody.velocity = new Vector3 (transform.rigidbody.velocity.x * 0.5f, transform.rigidbody.velocity.y * 1.1f, transform.rigidbody.velocity.z * 0.5f);
+				
+				
+				//Jump
+				if(MiddleVR.VRDeviceMgr.IsKeyPressed(MiddleVR.VRK_SPACE) == true)
+				{
+					rigidbody.AddForce(Vector3.up * jumpSpeed);
+				}
 			}
 		}
-		else
-		{
-			//Rotation
-			float rotationAxis = Input.GetAxis("Mouse X");
-			Debug.Log(rotationAxis.ToString());
-			//Regle le probleme d'hypersensibilite du controller
-			if(rotationAxis>sensibilite || rotationAxis<-sensibilite)
-				transform.Rotate(new Vector3 (0, rotationAxis, 0) * rotationSpeed);
-			
-			//Translation
-			float translationX = Input.GetAxis("Horizontal");
-			float translationZ = Input.GetAxis("Vertical");
-			//Regle le probleme d'hypersensibilite du controller
-			if((translationX>sensibilite || translationX<-sensibilite) || (translationZ>sensibilite || translationZ<-sensibilite))
-				transform.rigidbody.AddRelativeForce(new Vector3 (translationX, 0, translationZ) * moveSpeed);
-			else
-				transform.rigidbody.velocity = new Vector3 (transform.rigidbody.velocity.x * 0.5f, transform.rigidbody.velocity.y * 1.1f, transform.rigidbody.velocity.z * 0.5f);
-			
-			
-			//Jump
-			if(MiddleVR.VRDeviceMgr.IsKeyPressed(MiddleVR.VRK_SPACE) == true)
-			{
-				rigidbody.AddForce(Vector3.up * jumpSpeed);
-			}
-		}
+	}
+
+	public void setPause(bool p)
+	{
+		pause = p;
 	}
 }
---
+++ Assets/Script/Interface/Interface.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using System.Collections;
+//using System.Collections.Generic;
+
+public class Interface : MonoBehaviour {
+
+	//Variables
+	private bool interfaceOn;	//true -> interface activé, false -> interface désactivé
+	public float widthInterface;	//Largeur de l'interface
+	public float heightInterface;	//Hauteur de l'interface
+	private Rect windowRect;	//Taille de l'interface
+	private int curPosMenu;	//Position du curseur dans le menu
+	private bool xAxisStep;	//Si le stick est relaché ou non
+	private bool yAxisStep;	//Si le stick est relaché ou non
+	
+	public System.Collections.SortedList valeurMenu;	//Dictionnaire des options et de leur valeur correspondante
+
+	// Use this for initialization
+	void Start () {
+		interfaceOn = false;
+		xAxisStep = false;
+		yAxisStep = false;
+		windowRect = new Rect(Screen.width/2-widthInterface/2, Screen.height/2-heightInterface/2, widthInterface, heightInterface);
+
+		//Init menu
+		valeurMenu = new System.Collections.SortedList();
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		valeurMenu.Add ("Sensibilité vue", (int)player.GetComponent<moveController>().rotationSpeed);
+		valeurMenu.Add ("test bool", true);
+		valeurMenu.Add ("test int", 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Affiche le menu
+		if(MiddleVR.VRDeviceMgr.GetJoystick().IsButtonToggled(7))
+		{
+			interfaceOn = !interfaceOn;
+			GameObject player = GameObject.FindGameObjectWithTag("Player");
+			player.GetComponent<moveController>().setPause(interfaceOn);
+		}
+		if(interfaceOn == true)
+		{
+			//Input navigation
+			float xAxis = MiddleVR.VRDeviceMgr.GetJoystick().GetAxisValue(1);
+			if(Mathf.Abs(xAxis) > 0.5f)
+			{
+				if(xAxis < 0)
+					upDown(-1);
+				else
+					upDown(1);
+				xAxisStep = true;
+			}
+			else
+				xAxisStep = false;
+			float yAxis = MiddleVR.VRDeviceMgr.GetJoystick().GetAxisValue(0);
+			if(Mathf.Abs(yAxis) > 0.5f)
+			{
+				if(yAxis < 0)
+					rightLeft(-1);
+				else
+					rightLeft(1);
+				yAxisStep = true;
+			}
+			else
+				yAxisStep = false;
+		}
+	}
+
+	void OnGUI()
+	{
+		if(interfaceOn == true)
+		{
+			//On affiche le menu sur l'écran principal
+			if(MiddleVR.VRClusterMgr.IsServer())
+			{
+				windowRect = GUILayout.Window(0, windowRect, menu, "Menu");
+			}
+		}
+	}
+
+	void menu(int windowID)
+	{
+		//On parcourt toutes les cases du menu
+		for(int i=0; i<valeurMenu.Count; i++)
+		{
+			//Met en evidence la propriete en cours
+			if(curPosMenu == i)
+				GUI.color = Color.red;
+			else
+				GUI.color = Color.white;
+
+			GUILayout.BeginHorizontal();
+			GUILayout.Label(valeurMenu.GetKey(i).ToString());
+			GUILayout.FlexibleSpace();
+			//Agir de façon diffrente en fonction du type de la variable
+			if(valeurMenu.GetByIndex(i).GetType() == interfaceOn.GetType())
+			{
+				if((bool)valeurMenu.GetByIndex(i) == true)
+					GUILayout.Label("Activé");
+				else
+					GUILayout.Label("Désactivé");
+			}
+			else if(valeurMenu.GetByIndex(i).GetType() == curPosMenu.GetType())
+			{
+				GUILayout.Label(valeurMenu.GetByIndex(i).ToString());
+			}
+			GUILayout.EndHorizontal();
+		}
+	}
+
+	//Se dirige verticalement dans le menu
+	void upDown(int dir)
+	{
+		//Verification sortie menu
+		if(curPosMenu+dir<valeurMenu.Count && curPosMenu+dir>=0 && xAxisStep==false)
+			curPosMenu += dir;
+	}
+
+	//Se dirige horizontalement dans le menu
+	void rightLeft(int dir)
+	{
+		//Agir de façon diffrente en fonction du type de la variable
+		//Int
+		if(valeurMenu.GetByIndex(curPosMenu).GetType() == curPosMenu.GetType())
+		{
+			if(yAxisStep == false)
+			{
+				int newValue = (int)valeurMenu.GetByIndex(curPosMenu)+dir*10;
+				if(newValue<=100 && newValue>=0)
+					valeurMenu.SetByIndex(curPosMenu, newValue);
+			}
+		}
+		//Bool
+		else if(valeurMenu.GetByIndex(curPosMenu).GetType() == interfaceOn.GetType())
+		{
+			if(yAxisStep == false)
+			{
+				valeurMenu.SetByIndex(curPosMenu, !(bool)valeurMenu.GetByIndex(curPosMenu));
+			}
+		}
+
+		//Synchronise le parametre avec le programme
+		action ();
+	}
+
+	void action()
+	{
+		string param = (string)valeurMenu.GetKey (curPosMenu);
+		switch(param)
+		{
+		case "Sensibilité vue":
+			GameObject player = GameObject.FindGameObjectWithTag("Player");
+			player.GetComponent<moveController>().rotationSpeed = (int)valeurMenu.GetByIndex(curPosMenu);
+			break;
+		}
+	}
+}
